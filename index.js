3568d6c9ae06eb5fe96540371f804189dc77d55f
radheshyam545 <kumawatradhe545@gmail.com>
2025-03-18 22:24:44 +0530

    add new route

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello, World!" });
 });
 
+app.get('/radhe',(req,res)=>{
+  res.json({message:"hurrey chal gya re"})
+})
+
+app.post('/radhe',(req,res)=>{
+  res.json({message:"hurrey chal gya re"})
+})
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
